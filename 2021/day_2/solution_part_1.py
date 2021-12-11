39e460569a378b317992e806b4ca2079dc85b92c
David <davidcereijo@live.com>
2021-12-11 16:12:30 +0100

    Solved day 2 2021

---
+++ 2021/day_2/solution_part_1.py
@@ -0,0 +1,40 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.dataframe import DataFrame
+from pyspark.sql.functions import col, lag, monotonically_increasing_id, split, when
+from pyspark.sql.functions import sum as sum_spark
+from pyspark.sql.window import Window
+
+spark = SparkSession \
+    .builder \
+    .appName("Advent of Code 2021") \
+    .getOrCreate()
+
+df: DataFrame = spark.read.csv("2021/day_2/puzzle_input")
+
+df_with_contribs = df.select(
+  col("_c0").alias("raw_command"),
+  split(col("_c0"), " ").alias("command_parts")
+).select(
+  col("raw_command"),
+  col("command_parts").getItem(0).alias("command"),
+  col("command_parts").getItem(1).cast("integer").alias("argument")
+).select(
+  "*",
+  when(
+    col("command") == "forward",
+    col("argument")
+  ).otherwise(0).alias("horizontal_contribution"),
+  when(
+    col("command") == "down",
+    col("argument")
+  ).when(
+    col("command") == "up",
+    -col("argument")
+  ).otherwise(0).alias("depth_contribution")
+)
+
+df_with_contribs.agg(
+  sum_spark("horizontal_contribution"),
+  sum_spark("depth_contribution")
+).show()
+
